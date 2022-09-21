9efe080d7974b3e4d89405cdf4e2a0c8e5d6b94f
aalexff <alexfigueroahrdz@gmail.com>
2022-09-20 20:38:19 -0600

    Se crean los archivos fuentes necesarios.

---
+++ contador0A59V2.srcs/sources_1/new/topLevel.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/20/2022 07:51:23 PM
+// Design Name: 
+// Module Name: contador
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module topLevel(
+    input wire clk100MHz, reset,
+    output wire catodo, valor
+    );
+    
+    reg divContador, divMux;
+    reg [6:0] decenas;
+    reg [6:0] unidades;
+    
+    divisorDeFrecuencia divisorDeFrecuenciaInst(.divContador (clk100MHz), .divMux (clk100MHz));
+    contador contadorInst(.clk(divContador), .reset(reset), .bcd(decenas), .bcdu(unidades));
+    decoder decoderInst(.unidades(unidades), .decenas(decenas));
+    mux mixinst(.divMux(divMux));
+    
+endmodule
