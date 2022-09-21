9057f4089c69d75a9e9ff0b2d355134ce5b9304d
aalexff <alexfigueroahrdz@gmail.com>
2022-09-20 21:56:57 -0600

    divisorDeFrecuencia  V2.1.0
    Se crea un divisor de frecuencia para el contador.

--- contador0A59V2.srcs/sources_1/new/divisorDeFrecuencia.v
+++ contador0A59V2.srcs/sources_1/new/divisorDeFrecuencia.v
@@ -21,6 +21,24 @@
 
 
 module divisorDeFrecuencia(
-
+    input wire clk,
+    output reg clkdiv = 0, muxdiv = 0
     );
+    //Divisor de 1Hz
+    //El reloj interno de la Arty, es de 100MHz.
+    //Estos son, 100,000,000 cada segundo.
+    //Para almacenar ese valor, se requieren de 27 bits.
+    integer cuenta = 0;
+        
+    //Mientras no alcance los 100,000,000 ciclos de reloj, la cuenta segira aumentando.
+    //Para evitar retardos por desbordamiento, la cuenta se resetea a 0 y se enviara al cliclo de reloj
+    always@(posedge(clk))
+    begin
+        cuenta = cuenta + 1;
+        if (cuenta == 500000)
+        begin
+            clkdiv = ~clkdiv;
+            cuenta = 0;
+            end
+    end
 endmodule
